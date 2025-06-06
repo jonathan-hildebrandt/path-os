53dd342c91f499a81d63d3815101ca64293f8448
jonathan.hildebrandt <jonathan.hildebrandt.03@gmail.com>
2025-06-06 20:40:43 +0200

    chore: update package.json dependencies and entry point; add theme and styles files
    - Changed main entry point to "expo-router/entry".
    - Updated dependencies including expo-constants, expo-linking, expo-router, expo-system-ui, react-dom, react-native-safe-area-context, react-native-screens, react-native-web, and zustand.
    - Added new theme.ts file with light and dark theme definitions.
    - Added new styles.ts file.

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,77 @@
+import { Tabs } from 'expo-router';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useColorScheme } from 'react-native';
+import { darkTheme, lightTheme } from '../../theme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: lightTheme.primary,
+        tabBarInactiveTintColor: lightTheme.mutedForeground,
+        headerStyle: {
+          backgroundColor:
+            colorScheme === 'light'
+              ? lightTheme.background
+              : darkTheme.background,
+        },
+        headerShadowVisible: false,
+        headerTintColor:
+          colorScheme === 'light'
+            ? lightTheme.foreground
+            : darkTheme.foreground,
+        tabBarStyle: {
+          backgroundColor:
+            colorScheme === 'light'
+              ? lightTheme.background
+              : darkTheme.background,
+        },
+      }}>
+      <Tabs.Screen
+        name='index'
+        options={{
+          headerShown: false,
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'fitness' : 'fitness'}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='workouts'
+        options={{
+          title: 'Workouts',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'list' : 'list'}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='settings'
+        options={{
+          title: 'Settings',
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'settings-outline' : 'settings-outline'}
+              color={color}
+              size={24}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/workouts.tsx
@@ -0,0 +1,47 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View, StyleSheet, useColorScheme } from 'react-native';
+import { darkTheme, lightTheme, radius } from '../../theme';
+
+export default function WorkoutsScreen() {
+  const colorScheme = useColorScheme();
+
+  const themeTextStyle =
+    colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
+  const themeContainerStyle =
+    colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
+
+  return (
+    <View style={[styles.container, themeContainerStyle]}>
+      <Text style={[styles.text, themeTextStyle]}>Workouts Screen</Text>
+      <StatusBar />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: radius,
+  },
+  lightContainer: {
+    backgroundColor: lightTheme.background,
+  },
+  darkContainer: {
+    backgroundColor: darkTheme.background,
+  },
+  lightThemeText: {
+    color: lightTheme.foreground,
+    borderColor: lightTheme.border,
+  },
+  darkThemeText: {
+    color: darkTheme.foreground,
+    borderColor: darkTheme.border,
+  },
+});
