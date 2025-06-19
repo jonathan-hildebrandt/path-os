875e36b2aea7785cc6a9f73e3f70666a7d8c11ce
olli <oliver.schlalos@gmx.de>
2025-06-19 13:35:19 +0200

    added db.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,13 @@
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import initDatabase from '../db';
 
 export default function RootLayout() {
+
+  useEffect(() => {
+    initDatabase() 
+  }, []);
+  
   return (
     <Stack>
       <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
