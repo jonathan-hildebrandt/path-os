2c072708063f507909082230d5b8312b3e16122d
jonathan.hildebrandt <jonathan.hildebrandt.03@gmail.com>
2025-06-21 18:16:22 +0200

    style: format options for activity screen in RootLayout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,7 +12,10 @@ export default function RootLayout() {
       <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
       <Stack.Screen
         name='activity/[id]'
-        options={{ presentation: 'modal', headerShown: false }}
+        options={{
+          presentation: 'modal',
+          headerShown: false,
+        }}
       />
     </Stack>
   );
