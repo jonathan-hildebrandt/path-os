543ebfa1a6ec729830b70fcabae6ba001b5be4be
jonathan.hildebrandt <jonathan.hildebrandt.03@gmail.com>
2025-06-22 17:23:55 +0200

    feat: add TableRow component to display splits data in a structured format

--- components/splits.tsx
+++ components/splits.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { FlatList, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import { Run } from '../lib/model';
 import { darkTheme, lightTheme } from '../lib/theme';
 
@@ -12,9 +12,56 @@ export default function Splits({ run }: SplitsProps) {
   const themeTextStyle =
     colorScheme === 'light' ? styles.lightText : styles.darkText;
 
+  const TableRow = ({
+    km,
+    pace,
+    elevation,
+    isHeader = false,
+    isLast = false,
+  }: any) => (
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 8,
+        borderBottomWidth: isLast || isHeader ? 0 : 1,
+        borderColor:
+          colorScheme === 'light' ? lightTheme.border : darkTheme.border,
+      }}>
+      <Text style={{ fontWeight: isHeader ? 'bold' : 'normal', width: 60 }}>
+        {km}
+      </Text>
+      <Text style={{ fontWeight: isHeader ? 'bold' : 'normal', width: 80 }}>
+        {pace}
+      </Text>
+      <Text style={{ fontWeight: isHeader ? 'bold' : 'normal', width: 80 }}>
+        {elevation}
+      </Text>
+    </View>
+  );
+
   return (
     <View>
       <Text style={[styles.heading, themeTextStyle]}>Splits</Text>
+      <TableRow
+        km='Km'
+        pace='Avg. Pace'
+        elevation='Elevation'
+        isLast={false}
+        isHeader
+      />
+      <FlatList
+        data={run?.splits}
+        keyExtractor={(item) => item.km.toString()}
+        renderItem={({ item }) => (
+          <TableRow
+            km={item.km}
+            pace={item.splitPace}
+            elevation={item.splitElevation}
+            isLast={item.km === run?.splits[run.splits.length - 1].km}
+          />
+        )}
+      />
     </View>
   );
 }
